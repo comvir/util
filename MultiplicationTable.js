af69a9f246068765a790a8bcec51fde36884bd11
Comvir Lop <Comvir Lop>
2017-02-23 10:17:28 +0800

    add MultiplicationTable

---
+++ MultiplicationTable.js
@@ -0,0 +1,14 @@
+/**
+³Ë·¨±í
+*/
+var MultiplicationTable=function(level){
+	level=level||9;
+	for(var i=1;i<=9;i++){
+		var line=""
+		for(var j=1;j<=i;j++){
+			line+=i+"x"+j+"="+(i*j)+" ";
+		}
+		console.log(line);
+	}
+}
+MultiplicationTable();
