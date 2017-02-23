5f1d5bcd675f906c8cface5b22352a5338a24916
lopcomvir <lopcomvir@live.com>
2017-02-23 10:38:12 +0800

    Update MultiplicationTable.js

--- MultiplicationTable.js
+++ MultiplicationTable.js
@@ -1,9 +1,9 @@
 /**
-³Ë·¨±í
+ä¹˜æ³•è¡¨
 */
 var MultiplicationTable=function(level){
 	level=level||9;
-	for(var i=1;i<=9;i++){
+	for(var i=1;i<=level;i++){
 		var line=""
 		for(var j=1;j<=i;j++){
 			line+=i+"x"+j+"="+(i*j)+"\t";
