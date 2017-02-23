9108ffc64e7e0d043b5cfaae007be6d152832d0c
Comvir Lop <Comvir Lop>
2017-02-23 10:31:22 +0800

    some change

--- MultiplicationTable.js
+++ MultiplicationTable.js
@@ -6,7 +6,7 @@ var MultiplicationTable=function(level){
 	for(var i=1;i<=9;i++){
 		var line=""
 		for(var j=1;j<=i;j++){
-			line+=i+"x"+j+"="+(i*j)+" ";
+			line+=i+"x"+j+"="+(i*j)+"\t";
 		}
 		console.log(line);
 	}
